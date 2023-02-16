d1e39a7cd3777d7b1dfa8e9de32d6438a4f43d71
JamesDeChavez <jamesdechavez23@gmail.com>
2023-02-15 18:54:16 -0800

    played around with some animation ideas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+
+      <div className='name_container'>
+        <div className='J_container shape_container'>
+          <div className='J_innershape_hidden'></div>
+          <div className='J_innershape_visible'></div>
+        </div>
+        <h1 className='name_text'>ames</h1>
+      </div>
+
+      <div className='name_container'>
+        <div className='D_container shape_container'>
+          <div className='D_innershape_hidden'></div>
+          <div className='D_innershape_visible'></div>
+        </div>
+        <h1 className='name_text'>eChavez</h1>
+      </div>
+      
+      
     </div>
   );
 }
