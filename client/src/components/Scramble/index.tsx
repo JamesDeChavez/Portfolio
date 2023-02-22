1ea525855b29631c7c8af989c29629f3538b5770
JamesDeChavez <jamesdechavez23@gmail.com>
2023-02-22 13:40:42 -0800

    finished text scramble animation algorithm

--- client/src/components/Scramble/index.tsx
+++ client/src/components/Scramble/index.tsx
@@ -1,66 +1,109 @@
 import { useEffect, useRef, useState } from 'react';
 import { randomSelectFromArray } from '../../utils/functions';
-import { useInterval } from '../../utils/hooks';
 import './styles.css'
 
 const Scramble = () => {
-    const wordsToScramble = ['Web Developer', 'Problem Solver Testing Testing', 'Efficiency Enthusiast', 'Data Driven']
-    const charsForScramble = 'MHLUIKWjxqzcu!<>+_?@#$%&()'
+    const wordsToScramble = ['Fullstack Developer', 'Problem Solver', 'Automation Addict', 'Data Driven']
+    const charsForScramble = '!<>+_?@#$%&()'
 
-    const [currentWord, setCurrentWord] = useState<string[]>(wordsToScramble[0].split(''))
+    const [currentWord, setCurrentWord] = useState<string[]>([])    
+    const [wordToRender, setWordToRender] = useState<string[]>(Array(wordsToScramble[0].length).fill('_'))
+    const [indexesLeftToUpdate, setIndexesLeftToUpdate] = useState<number[]>([])
     
-    const initialScramble = Array(wordsToScramble[0].length).fill('_')
-        // .map(() => randomSelectFromArray(charsForScramble.split('')))
-
-    const [wordToRender, setWordToRender] = useState<string[]>(initialScramble)
-
-    const indexesLeftToUpdate: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     const populateIndexesArray = () => {
-        wordToRender.forEach((_, i) => {
-            indexesLeftToUpdate.push(i)
+        let updatedIndexesArray: number[] = []
+        currentWord.forEach((_, i) => {
+            updatedIndexesArray.push(i)
         })
+        setIndexesLeftToUpdate(updatedIndexesArray)
+    }
+
+    const nextWord = () => {
+        let currentIndex = wordsToScramble.indexOf(currentWord.toString().replaceAll(',', ''))
+        let nextIndex = (currentIndex + 1) % wordsToScramble.length
+        setCurrentWord(wordsToScramble[nextIndex].split(''))
     }
 
     const savedCallback: any = useRef()
     const savedScramble: any = useRef()
     const savedIndexes: any = useRef()
+    let intervalId: NodeJS.Timer
 
-    const updateScrambleCallback = () => {        
+    const updateScrambleCallback = (interval: NodeJS.Timer) => {
+        //Pull Reference Variables        
         let updatedWord: string[] = [...savedScramble.current]
         let indexOptions: number[] = [...savedIndexes.current]
-        
-        let randomIndex: number = randomSelectFromArray(indexOptions)
-        let randomChar: string = randomSelectFromArray(charsForScramble.split(''));
-        updatedWord[randomIndex] = randomChar
-        
+
+        //Check if you can exit interval
+        if (indexOptions.length === 0) {
+            setTimeout(() => {
+                nextWord()
+            }, 1000)
+            clearInterval(interval)
+        }
+
+        //Randomly choose up to 3 indexes and change them to random chars
+        let numOfLoops = randomSelectFromArray([1,2,3])
+        for (let i = 0; i < numOfLoops; i++) {
+            let randomIndex: number = randomSelectFromArray(indexOptions)
+            let randomChar: string = randomSelectFromArray(charsForScramble.split(''));
+            updatedWord[randomIndex] = randomChar
+        }
+
+        //Randomly choose 1 index and change it to the correct char
         let anotherRandomIndex: number = randomSelectFromArray(indexOptions)
         updatedWord[anotherRandomIndex] = currentWord[anotherRandomIndex]
-        
-        indexOptions.splice(anotherRandomIndex, 1)
-        savedIndexes.current = [...indexOptions]
-        console.log(indexOptions)
-        
 
+        //Remove the correct index from the list of options
+        indexOptions.splice(indexOptions.indexOf(anotherRandomIndex), 1)
+
+        //Check if Array size needs DECREASE
+        if (updatedWord.length > currentWord.length) {
+            updatedWord.pop()
+        }
+        while (updatedWord.length > currentWord.length && indexOptions.length === 1) {
+            updatedWord.pop()
+        }        
+
+        //Check if Array size needs to INCREASE
+        if (updatedWord.length < currentWord.length) {
+            let randomChar: string = randomSelectFromArray(charsForScramble.split(''));
+            updatedWord.push(randomChar)
+            indexOptions.push(updatedWord.length - 1)
+            
+        }
+        
         //Update State
+        setIndexesLeftToUpdate([...indexOptions])
         setWordToRender(updatedWord)
     }
 
     useEffect(() => {
-        savedIndexes.current = [...indexesLeftToUpdate]
+        setTimeout(() => {
+            setCurrentWord(wordsToScramble[0].split('')) 
+        }, 1000) 
     }, [])
-
+    
     useEffect(() => {
         savedCallback.current = updateScrambleCallback
+    })
+
+    useEffect(() => {
         savedScramble.current = [...wordToRender]
     }, [wordToRender])
 
     useEffect(() => {
-        function tick() {
-            savedCallback.current()
+        savedIndexes.current = [...indexesLeftToUpdate]
+    }, [indexesLeftToUpdate])
+
+    useEffect(() => {
+        populateIndexesArray()
+        function scramble() {
+            savedCallback.current(intervalId)
         }
-        let intervalId = setInterval(tick, 100)
+        intervalId = setInterval(scramble, 100)
         return () => clearInterval(intervalId)
-    }, [])
+    }, [currentWord])
 
     const className = 'Scramble';
     return (
