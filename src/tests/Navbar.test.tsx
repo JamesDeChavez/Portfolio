19ae543f95611b6656a5de2a27d525280a8c2121
JamesDeChavez <jamesdechavez23@gmail.com>
2023-04-14 23:10:40 -0700

    added navbar testing

--- src/tests/Navbar.test.tsx
+++ src/tests/Navbar.test.tsx
@@ -1,11 +1,30 @@
-import { render, screen, act } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen, act, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Navbar from '../components/Navbar'
 
 describe('Navbar', () => {
-    it('should render hamburger button', () => {
+    it('should render hamburger button and be visible', () => {
         act(() => render(<Navbar/>))
         const hamburgerElement = screen.getByTitle('Burger Icon Button')
-        expect(hamburgerElement).toBeInTheDocument
+        expect(hamburgerElement).toBeInTheDocument()
+        expect(hamburgerElement).toBeVisible()
+    })
+    it('should render topRow', () => {
+        act(() => render(<Navbar/>))
+
+        const topRowElement = screen.getByRole('button', { name: /James DeChavez/i })
+        expect(topRowElement).toBeInTheDocument()
+    })
+    it('should render nav buttons', () => {
+        act(() => render(<Navbar/>))
+
+        const homeNavButton = screen.getByRole('button', { name: /Home/i })
+        const aboutNavButton = screen.getByRole('button', { name: /About/i })
+        const projectsNavButton = screen.getByRole('button', { name: /Projects/i })
+        const contactNavButton = screen.getByRole('button', { name: /Contact/i })
+        expect(homeNavButton).toBeInTheDocument()
+        expect(aboutNavButton).toBeInTheDocument()
+        expect(projectsNavButton).toBeInTheDocument()
+        expect(contactNavButton).toBeInTheDocument()
     })
 })
